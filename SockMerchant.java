06b5c8ecaf39d7564c7a53c5b81ddbd4cf0f0223
ABHAY MUDGAL <52732872+mudgalabhay@users.noreply.github.com>
2019-11-23 22:21:47 +0530

    Update and rename StockMerchant.java to SockMerchant.java

--- SockMerchant.java
+++ SockMerchant.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StockMerchant {
+public class SockMerchant {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
